40c4222aaaf5a3e10d4650aad34db8af2da79a11
Paddle CI <paddle_ci@example.com>
2018-11-12 17:33:30 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python bin/train_elmo.py --train_prefix='./baike/train/sentence_file_15519.txt' --vocab_file ./baike/vocabulary_min10.txt --save_dir output/checkout/
+export CUDA_VISIBLE_DEVICES=0
+python -m pdb bin/train_elmo.py --train_prefix='./baike/train/sentence_file_15519.txt' --vocab_file ./baike/vocabulary_min10.txt --save_dir output/checkout/
