eeb0c3f35024cee43a9f8cd56205fc7588a81f33
Paddle CI <paddle_ci@example.com>
2018-12-25 20:31:57 +0800

    fix bug

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export PYTHONPATH=$PYTHONPATH:.
+export CUDA_VISIBLE_DEVICES=0
 python  bin/train_elmo.py --train_prefix='./baike/train/sentence_*.txt' --vocab_file ./baike/vocabulary_min5k.txt --save_dir output/checkout/ $@
